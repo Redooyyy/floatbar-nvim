7794aee0808a4ad03299fef3c2f9356019050fd1
Redooyyy <rwarieal@gmail.com>
2026-01-09 18:41:14 +0600

    final fix

--- plugin/floatbar.lua
+++ plugin/floatbar.lua
@@ -4,7 +4,14 @@ if vim.g.loaded_floatbar then
 end
 vim.g.loaded_floatbar = true
 
--- Default toggle keymap
+-- Normal mode toggle
 vim.keymap.set('n', '<space>tt', function()
   require('floatbar').toggle()
 end, { desc = 'Toggle Floating Terminal' })
+
+-- Terminal mode toggle
+vim.keymap.set('t', '<space>tt', function()
+  -- First switch to terminal-normal mode, then toggle
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<C-\\><C-n>', true, false, true), 'n', true)
+  require('floatbar').toggle()
+end, { desc = 'Toggle Floating Terminal from terminal mode' })
