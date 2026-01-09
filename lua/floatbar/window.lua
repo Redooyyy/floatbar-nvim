e492a38305f21b5d5d8d935859dde0ec98f14ca6
Redooyyy <rwarieal@gmail.com>
2026-01-09 18:09:03 +0600

    fixing

--- lua/floatbar/window.lua
+++ lua/floatbar/window.lua
@@ -9,7 +9,7 @@ local config = {
   width = 0.8, -- 80% of screen
   height = 0.8, -- 80% of screen
   border = 'rounded',
-  winblend = 10, -- semi-transparent
+  winblend = 20, -- semi-transparent
 }
 
 -- Allow init.lua to update config
@@ -45,32 +45,42 @@ end
 
 -- Open floating terminal
 function M.open()
-  vim.o.termguicolors = true -- enable true colors
+  -- Enable true colors for theme support
+  vim.o.termguicolors = true
 
+  -- Create or focus existing window
   if term_win and vim.api.nvim_win_is_valid(term_win) then
     vim.api.nvim_set_current_win(term_win)
     return
   end
 
+  -- Create buffer if it doesn't exist
   if not term_buf or not vim.api.nvim_buf_is_valid(term_buf) then
     term_buf = vim.api.nvim_create_buf(false, true)
     vim.api.nvim_buf_set_option(term_buf, 'bufhidden', 'hide')
   end
 
+  -- Open floating window
   term_win = vim.api.nvim_open_win(term_buf, true, float_config())
 
+  -- Start terminal if not already a terminal buffer
   if vim.bo[term_buf].buftype ~= 'terminal' then
     vim.fn.termopen(vim.o.shell)
   end
 
-  -- Match Neovim theme and optionally transparent
-  vim.api.nvim_win_set_option(term_win, 'winhl', 'Normal:Normal,NormalNC:Normal,FloatBorder:Normal')
+  -- Modern transparent / theme-aware highlights
+  -- Set NormalFloat and FloatBorder to transparent if not already defined
+  vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'NONE' })
+  vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'NONE' })
+
+  -- Apply to floating terminal window
+  vim.api.nvim_win_set_option(term_win, 'winhl', 'Normal:NormalFloat,FloatBorder:FloatBorder')
   vim.api.nvim_win_set_option(term_win, 'winblend', config.winblend)
 
   vim.cmd('startinsert')
 end
 
--- Close floating terminal but keep buffer
+-- Close floating terminal but keep buffer alive
 function M.close()
   if term_win and vim.api.nvim_win_is_valid(term_win) then
     vim.api.nvim_win_close(term_win, false)
