6e0cb6ea248aa1bcb41291e480143b144efb7493
Redooyyy <rwarieal@gmail.com>
2026-01-22 13:28:13 +0600

    Improve for user define keybinding rather then hardcoded keybinding

--- plugin/floatbar.lua
+++ plugin/floatbar.lua
@@ -1,17 +1,35 @@
--- Prevent multiple loads
-if vim.g.loaded_floatbar then
-  return
+local M = {}
+
+local defaults = {
+  keymaps = {
+    toggle = '<C-t>',
+  },
+}
+
+local config = defaults
+
+function M.setup(user_config)
+  config = vim.tbl_deep_extend('force', defaults, user_config or {})
+
+  -- user explicitly disables keymaps
+  if not config.keymaps or not config.keymaps.toggle then
+    return
+  end
+
+  -- Normal mode
+  vim.keymap.set('n', config.keymaps.toggle, function()
+    require('floatbar').toggle()
+  end, { desc = 'Toggle Floating Terminal' })
+
+  -- Terminal mode
+  vim.keymap.set('t', config.keymaps.toggle, function()
+    vim.api.nvim_feedkeys(
+      vim.api.nvim_replace_termcodes('<C-\\><C-n>', true, false, true),
+      'n',
+      true
+    )
+    require('floatbar').toggle()
+  end, { desc = 'Toggle Floating Terminal (terminal mode)' })
 end
-vim.g.loaded_floatbar = true
 
--- Normal mode toggle
-vim.keymap.set('n', '<C-t>', function()
-  require('floatbar').toggle()
-end, { desc = 'Toggle Floating Terminal' })
-
--- Terminal mode toggle
-vim.keymap.set('t', '<C-t>', function()
-  -- First switch to terminal-normal mode, then toggle
-  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<C-\\><C-n>', true, false, true), 'n', true)
-  require('floatbar').toggle()
-end, { desc = 'Toggle Floating Terminal from terminal mode' })
+return M
