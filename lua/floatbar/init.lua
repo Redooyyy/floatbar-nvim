84504f180bf3a17c618cf4ba3bea9180cd5082e5
Redooyyy <rwarieal@gmail.com>
2026-01-22 14:31:35 +0600

    Tried something

--- lua/floatbar/init.lua
+++ lua/floatbar/init.lua
@@ -7,6 +7,30 @@ function M.setup(user_config)
   if user_config then
     window.set_config(user_config)
   end
+
+  -- Setup keymaps after config is set
+  if not vim.g.loaded_floatbar then
+    vim.g.loaded_floatbar = true
+
+    local toggle_key = window.config.keymaps.toggle or '<C-t>'
+
+    -- Normal mode toggle
+    vim.keymap.set('n', toggle_key, function()
+      window.toggle()
+    end, {
+      desc = 'Toggle Floating Terminal',
+    })
+
+    -- Terminal mode toggle
+    vim.keymap.set('t', toggle_key, function()
+      vim.api.nvim_feedkeys(
+        vim.api.nvim_replace_termcodes('<C-\\><C-n>', true, false, true),
+        'n',
+        true
+      )
+      window.toggle()
+    end, { desc = 'Toggle Floating Terminal from terminal mode' })
+  end
 end
 
 -- Public API
