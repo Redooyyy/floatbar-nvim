b4336c4f8c5813fbc76284496c1e631132135df4
Redooyyy <rwarieal@gmail.com>
2026-01-22 13:42:37 +0600

    What the hell is wrong with me -_-

--- lua/floatbar/init.lua
+++ lua/floatbar/init.lua
@@ -1,5 +1,5 @@
 local window = require('floatbar.window')
-local keymaps = require('lua.plugin.floatbar')
+local keymaps = require('floatbar.floatbar')
 local M = {}
 
 -- Setup function for user config
