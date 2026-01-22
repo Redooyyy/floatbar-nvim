1050581bdc90b5ea7ed93c300ae16326e4fa1e50
Redooyyy <rwarieal@gmail.com>
2026-01-22 13:35:59 +0600

    Flash fix

--- lua/floatbar/init.lua
+++ lua/floatbar/init.lua
@@ -1,11 +1,12 @@
 local window = require('floatbar.window')
-
+local keymaps = require('plugin.floatbar')
 local M = {}
 
 -- Setup function for user config
 function M.setup(user_config)
   if user_config then
     window.set_config(user_config)
+    keymaps.setup(user_config)
   end
 end
 
